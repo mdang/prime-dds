40c8ece81152d5b1d27cd9c78290afaf3a7b1d6d
johanwin <whatsup@johnnguyen.org>
2020-08-18 15:23:05 -0500

    add robots.txt to disallow crawling

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://mdang.github.io/prime-dds/',
+        policy: [{ userAgent: '*', disallow: '/' }]
+      }
+    }
   ],
 }
