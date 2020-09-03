0f1ecc55eb64a1b3ce562989afa94abbc5b35d59
johanwin <whatsup@johnnguyen.org>
2020-09-02 20:46:41 -0500

    putting initial seperate pages

---
+++ src/pages/arlingtonDental.js
@@ -0,0 +1,39 @@
+import React from "react"
+import SEO from "../components/seo"
+import "bootstrap/dist/css/bootstrap.css"
+
+import Layout from "../containers/Layout"
+import Content from "../containers/Content"
+import Section from "./section"
+import Safe from "../containers/Safe"
+import Service from "../containers/Service"
+import AboutUs from "../containers/AboutUs"
+import WebsiteDesc from "../containers/WebsiteDesc"
+import Team from "../containers/Team"
+import TriColumn from "../containers/TriColumn"
+import GetStart from "../containers/GetStart"
+import Contact from "../containers/Contact"
+import Social from "../containers/Social"
+
+const IndexPage = () => {
+  return (
+    <Layout typeOfPage="arlingtonDental" themeColor="theme-color-black">
+      <SEO title="" description="" />
+      <Content>
+        <Section typeOfPage="clinic" />
+        <Safe />
+        <TriColumn />
+        <Team />
+        <AboutUs />
+        <WebsiteDesc />
+        <Service />
+        <Contact />
+        <GetStart />
+        <Social />
+      </Content>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
