376b1dc01b3660baee387f3f6341e76953e61d48
johanwin <whatsup@johnnguyen.org>
2020-09-23 21:03:33 -0500

    re-added tricolumn

--- src/containers/TriColumn/index.js
+++ src/containers/TriColumn/index.js
@@ -6,19 +6,52 @@ function TriColumn(props) {
   let TriColumnDataPrefix = TriColumnDataWhiteRock
   return (
     <section className="section section-lg section-tri">
-      <div className="bg-overlay"></div>
       <Container>
         <Row>
-          <Col lg={12}>
-            
-            <h2 className="get-started-title text-white mb-5">
-            <i className={TriColumnDataPrefix[0].subheadline + " text-white padding-t-30"}></i>
+          <Col lg={4}>
+            <div className="get-started-title-rating">{TriColumnDataPrefix[0].rating}</div>
+            <h2 className="get-started-title">
               {TriColumnDataPrefix[0].title}
             </h2>
+            <p className="section-subtitle-cta text-muted font-secondary padding-t-30">
+              {TriColumnDataPrefix[0].subheadline}
+            </p>
+            <a
+              href="#"
+              className="btn btn-custom margin-t-30 waves-effect waves-light mb-5"
+            >
+              {TriColumnDataPrefix[0].button}
+            </a>
+          </Col>
+          <Col lg={4}>
+          <i className={TriColumnDataPrefix[1].icon + " text-muted padding-t-30"}></i>
+            <h2 className="get-started-title-cta">
+              {TriColumnDataPrefix[1].title}
+            </h2>
+            <p className="section-subtitle-cta text-muted font-secondary padding-t-30">
+              {TriColumnDataPrefix[1].subheadline}
+            </p>
+            <a
+              href="#"
+              className="btn btn-custom margin-t-30 waves-effect waves-light mb-5"
+            >
+              {TriColumnDataPrefix[1].button}
+            </a>
+          </Col>
+          <Col lg={4}>
+            <i className={TriColumnDataPrefix[2].icon + " text-muted padding-t-30"}></i>
+            <h2 className="get-started-title">
+              {TriColumnDataPrefix[2].title}
+            </h2>
+            <a
+              href="#"
+              className="btn btn-custom margin-t-30 waves-effect waves-light mb-5"
+            >
+              {TriColumnDataPrefix[2].button}
+            </a>
           </Col>
         </Row>
       </Container>
-      <div className="bg-pattern-effect"></div>
     </section>
   )
 }
--- src/data/triColumn/whiteRock/index.js
+++ src/data/triColumn/whiteRock/index.js
@@ -1,10 +1,23 @@
 const TriColumnData = [
-    {
-      title: "$19 FIRST EXAM AND X-RAYS",
-      subheadline: "flaticon-085-monitor",
-      button: "Read Reviews"
-    }
-  ]
-  
-  export default TriColumnData
-  
+  {
+    title: "AVERAGE RATING",
+    subheadline: "Over 200 reviews.",
+    rating: "4.8",
+    icon: "flaticon-085-monitor",
+    button: "Read Reviews"
+  },
+  {
+    title: "$19 First Exam",
+    subheadline: "+ X-Rays",
+    icon: "flaticon-085-monitor",
+    button: "Get your Coupon Today"
+  },
+  {
+    title: "Saturday & emergency appointments available.",
+    subheadline: "Proin sed dui nec ipsum mollis venenatis nec id dui.",
+    icon: "flaticon-097-calendar",
+    button: "Book Now"
+  },
+]
+
+export default TriColumnData
