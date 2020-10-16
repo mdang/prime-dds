6686c7231ffaca05499d331089f2ce83905515f2
johanwin <whatsup@johnnguyen.org>
2020-10-15 19:55:40 -0500

    remove analytics

--- src/data/triColumn/share/index.js
+++ src/data/triColumn/share/index.js
@@ -18,7 +18,7 @@ const TriColumnData = [
     subheadline: "Proin sed dui nec ipsum mollis venenatis nec id dui.",
     icon: "flaticon-097-calendar",
     button: "Book Now",
-    link: "https://www.localmed.com/offices/9vfgqmjut6k5kv5_026b3ce/widget/?chrome=&hidePartnerLogo=true&partnerId=&referringWidgetId=986452ac-5caa-40f4-b176-5a480ac892f1&scheduling=in_widget&utm_campaign=button_widget&utm_content=Share%20Dentistry%20Google&utm_medium=widget&utm_source=www.google.com&patientStatusId=new&reasonForVisitId=&after=2020-10-14&minDate=2020-10-14"
+    link: "https://www.localmed.com/offices/9vfgqmjut6k5kv5_026b3ce/"
   },
 ]
 
